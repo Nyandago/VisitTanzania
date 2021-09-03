52eb60d93171439a3abc5924430cc1c6d6fd3dbf
nyandago <paulmutator@gmail.com>
2021-09-03 18:58:10 +0300

    List View Layout Design

---
+++ app/src/main/java/com/cannybits/visittanzania/DestinationTz.kt
@@ -0,0 +1,13 @@
+package com.cannybits.visittanzania
+
+class DestinationTz {
+    var name: String? = null
+    var description: String? = null
+    var image: Int? = null
+
+    constructor(nm: String, desc: String, img: Int){
+        this.name = nm
+        this.description = desc
+        this.image = img
+    }
+}
