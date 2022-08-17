d70f8eb7dd5f9caaeb9fd91d92059c5cf7c4db20
Nyandago <paulmutator@gmail.com>
2022-08-17 13:02:35 +0300

    some modifications

--- app/src/main/java/com/cannybits/visittanzania/MainActivity.kt
+++ app/src/main/java/com/cannybits/visittanzania/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.cannybits.visittanzania
 
-import android.content.Context
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.*
 
 
